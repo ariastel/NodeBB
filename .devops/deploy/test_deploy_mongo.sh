e1ece18ebed78314434bcd1d390a92b0706c7cf4
Konstantin Kozhin <1387510+kozhin@users.noreply.github.com>
2021-03-28 15:14:56 +0300

    Minor fixes

--- .devops/deploy/test_deploy_mongo.sh
+++ .devops/deploy/test_deploy_mongo.sh
@@ -10,7 +10,7 @@ helm install mongodb-production bitnami/mongodb \
   --set arbiter.enabled=true
 
 #
-# Create MongoDB database and add a new user
+# Create MongoDB a database and add a new user
 #
 # 1. kubectl run --namespace production mongodb-production-client --rm --tty -i --restart='Never' --env="MONGODB_ROOT_PASSWORD=$MONGODB_ROOT_PASSWORD" --image docker.io/bitnami/mongodb:4.4.4-debian-10-r41 --command -- bash
 # 2. mongo admin --host "mongodb-production-0.mongodb-production-headless.production.svc.cluster.local:27017,mongodb-production-1.mongodb-production-headless.production.svc.cluster.local:27017,mongodb-production-2.mongodb-production-headless.production.svc.cluster.local:27017"
