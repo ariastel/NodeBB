1a60c6736ffbf39ac4d5601b69e847490513a7e7
Konstantin Kozhin <1387510+kozhin@users.noreply.github.com>
2021-03-28 17:36:06 +0300

    Minor fixes

--- .devops/build/k8s.start.sh
+++ .devops/build/k8s.start.sh
@@ -3,8 +3,11 @@
 echo "Creating nodebb config.json from envs..."
 envsubst < /opt/nodebb/k8s.config.json.template > /opt/nodebb/config.json || (echo "Unable to create nodebb config.json" && exit 1)
 
-# echo "Upgrading nodebb..."
+# Run command below if it's a fresh new install
+# ./nodebb setup
+
+# Run command below for upgrade
 # ./nodebb upgrade
 
 echo "Starting nodebb..."
-node ./nodebb start
+./nodebb start
